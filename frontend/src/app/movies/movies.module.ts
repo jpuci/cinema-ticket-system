6820a1bace7d9acaf8d844c96ba5f7f4c6db9aca
Joanna Puciłowska <jpucilowska@tlen.pl>
2023-05-10 19:45:47 +0200

    Create initial version of movie list view

--- frontend/src/app/movies/movies.module.ts
+++ frontend/src/app/movies/movies.module.ts
@@ -5,18 +5,22 @@ import {HttpClientModule} from "@angular/common/http";
 import {RouterModule} from "@angular/router";
 import {MovieDetailsComponent} from "./components/movie-details/movie-details.component";
 import {ReactiveFormsModule} from "@angular/forms";
+import { NavigationComponent } from './components/navigation/navigation.component';
+import {MatIconModule} from "@angular/material/icon";
 // import movies from "/backend/movies.json"
 
 @NgModule({
   declarations: [
     MovieListComponent,
     MovieDetailsComponent,
+    NavigationComponent,
   ],
   imports: [
     CommonModule,
     HttpClientModule,
     RouterModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatIconModule
   ]
 })
 export class MoviesModule { }
