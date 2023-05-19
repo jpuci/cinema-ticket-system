12818f5fba563d906accd3e862d63d8e0b72f6c2
Bogusława Tlołka <bogusia29.11@gmail.com>
2023-05-20 00:11:57 +0200

    Remove unused imports

--- backend/src/main/java/com/cinema/cinema/controller/HallsController.java
+++ backend/src/main/java/com/cinema/cinema/controller/HallsController.java
@@ -1,9 +1,7 @@
 package com.cinema.cinema.controller;
 
 import com.cinema.cinema.model.Halls;
-import com.cinema.cinema.model.Movie;
 import com.cinema.cinema.service.HallsService;
-import com.cinema.cinema.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
